0eb9e03cc4c0ee1e8022380fcb7339b48eae916a
JanethRodriguezR <orodriguez341@unab.edu.co>
2020-08-21 09:45:54 -0500

    final

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 let puntaje = 0;
+let mensaje = "";
 
 function juego(a) {
+    document.getElementById("inicio_juego").style.display = "none";
+    document.getElementById("resultado_parcial").style.display = "flex";
+    //mostrar resultado final
+    setTimeout(function() {
+        document.getElementById("resultado").style.display = "flex";
+        document.getElementById("resultado_parcial").style.display = "none";
+    }, 1500);
+    //reiniciar juego con el boton
+    document.getElementById("btn").addEventListener("click", () => {
+        document.getElementById("inicio_juego").style.display = "block";
+        document.getElementById("resultado").style.display = "none";
+        //mostrar de nuevo circulo 
+        var circulo = "<div id='nada'></div>";
+        document.getElementById('pc_picked1').innerHTML = circulo;
+    });
+
     var jugador = ["piedra", "papel", "tijera"];
     var jugada = [
         ["Empate", "Perdiste", "Ganaste"],
@@ -8,21 +25,67 @@ function juego(a) {
         ["Perdiste", "Ganaste", "Empate"]
     ];
     var computador = Math.floor(Math.random() * 3);
-    document.getElementById('persona').innerHTML = jugador[a];
-    document.getElementById('pc').innerHTML = jugador[computador];
-    document.getElementById('resultado').innerHTML = jugada[a][computador];
 
-    if (jugada[a][computador] == "Ganaste") {
-        console.log(jugada[a][computador]);
-        puntaje++;
+    //PUNTAJES
+    setTimeout(function() {
+        if (jugada[a][computador] == "Ganaste") {
+            mensaje = "YOU WIN";
+            puntaje++;
+            document.getElementById("col_pc").className = "col";
+            document.getElementById("col_u").className += " col_ganador";
+        }
+        if (jugada[a][computador] == "Perdiste") {
+            mensaje = "YOU LOST";
+            puntaje = puntaje - 1;
+            document.getElementById("col_pc").className += " col_ganador";
+            document.getElementById("col_u").className = "col";
+        }
+        if (jugada[a][computador] == "Empate") {
+            mensaje = "EQUALS";
+            puntaje = puntaje;
+            document.getElementById("col_u").className = "col";
+            document.getElementById("col_pc").className = "col";
+        }
+        document.getElementById('puntos').innerHTML = puntaje;
+        document.getElementById('msj_resultado').innerHTML = mensaje;
+    }, 1500);
+    //VISTA 2 y 3  
+    //opciones persona
+    if (a == 0) {
+        var tu_eleccion = document.getElementById('piedra');
+        document.getElementById('you_picked1').innerHTML = tu_eleccion.outerHTML;
+        document.getElementById('you_picked').innerHTML = tu_eleccion.outerHTML;
     }
-    if (jugada[a][computador] == "Perdiste") {
-        console.log(jugada[a][computador]);
-        puntaje = puntaje - 1;
+    if (a == 1) {
+        var tu_eleccion = document.getElementById('papel');
+        document.getElementById('you_picked1').innerHTML = tu_eleccion.outerHTML;
+        document.getElementById('you_picked').innerHTML = tu_eleccion.outerHTML;
     }
-    if (jugada[a][computador] == "Empate") {
-        console.log(jugada[a][computador]);
-        puntaje = puntaje;
+    if (a == 2) {
+        var tu_eleccion = document.getElementById('tijera');
+        document.getElementById('you_picked1').innerHTML = tu_eleccion.outerHTML;
+        document.getElementById('you_picked').innerHTML = tu_eleccion.outerHTML;
     }
-    document.getElementById('puntos').innerHTML = puntaje;
+    //opciones pc
+    if (computador == 0) {
+        var pc_eleccion = document.getElementById('piedra');
+        setTimeout(function() {
+            document.getElementById('pc_picked1').innerHTML = pc_eleccion.outerHTML;
+        }, 1000);
+        document.getElementById('pc_picked').innerHTML = pc_eleccion.outerHTML;
+    }
+    if (computador == 1) {
+        var pc_eleccion = document.getElementById('papel');
+        setTimeout(function() {
+            document.getElementById('pc_picked1').innerHTML = pc_eleccion.outerHTML;
+        }, 1000);
+        document.getElementById('pc_picked').innerHTML = pc_eleccion.outerHTML;
+    }
+    if (computador == 2) {
+        var pc_eleccion = document.getElementById('tijera');
+        setTimeout(function() {
+            document.getElementById('pc_picked1').innerHTML = pc_eleccion.outerHTML;
+        }, 1000);
+        document.getElementById('pc_picked').innerHTML = pc_eleccion.outerHTML;
+    }
 }
